bd5038ae71303ba554ac48c28bc7ed41dc4557c4
vamshiyadav-06 <vanshiyadav245@gmail.com>
2026-05-27 17:10:56 +0530

    the updated code with fixing the database conn

--- main.py
+++ main.py
@@ -22,14 +22,15 @@ app.add_middleware(
 # ==========================
 
 def get_db_connection():
+    
 
     connection = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306))
-    )
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    port=int(os.getenv("DB_PORT", 3306))
+)
 
     return connection
 
